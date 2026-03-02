8afe98e98de670841118c9d6f025af2532692baa
Amit-jpg18 <amitpunase4@gmail.com>
2026-03-02 16:12:03 +0530

    solve cors problem

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const cors = require("cors");
+
 
 app.use(cors({
   origin: [
